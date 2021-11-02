9fa537586aa799e1586b8bdb4c8d14ac899e6090
SimonvanStrijp <36440541+SimonvanStrijp@users.noreply.github.com>
2021-11-02 13:00:41 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let about = document.querySelector(".about")
+about.addEventListener("click",function(){
+    var element = document.getElementById("menu");
+    element.classList.toggle("toggle")
+
+})
